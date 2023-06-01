6b2b4fbe6ffdbd182b27e5a160ffdffffc2b28b7
Adam Dymitruk <adam.dymitruk@adaptechgroup.com>
2023-06-01 00:47:57 -0700

    change syntax to bash

--- switch_terminal_title.sh
+++ switch_terminal_title.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/fish
+#!/bin/bash
 
 # Function to change the terminal window title in Konsole
-function change_window_title
-    printf "\033]30;$argv[1]\a"
-end
+function change_window_title() {
+  echo -ne "\033]30;"$1"\007"
+}
 
 # Prompt and change the title to "Script Start"
-read -P "Press Enter to start the script..."
+read -p "Press Enter to start the script..."
 change_window_title "Script Start"
 echo "Terminal window title changed to 'Script Start'"
 
 # Your script code goes here
 
 # Prompt and change the title to "Script in Progress"
-read -P "Press Enter to continue..."
+read -p "Press Enter to continue..."
 change_window_title "Script in Progress"
 echo "Terminal window title changed to 'Script in Progress'"
 
 # More script code
 
 # Prompt and change the title to "Script Completed"
-read -P "Press Enter to finish the script..."
+read -p "Press Enter to finish the script..."
 change_window_title "Script Completed"
 echo "Terminal window title changed to 'Script Completed'"
 
